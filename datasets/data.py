b2d268d2fe719b0e3fcb1dee4bb73cb71b919918
JongsooKeum <keum.jongsoo@sualab.com>
2018-03-28 13:53:52 +0900

    add load data

--- datasets/data.py
+++ datasets/data.py
@@ -2,5 +2,182 @@ import os
 import numpy as np
 from skimage.io import imread
 from skimage.transfrom import resize
+import glob
+import json
 
-def read_data(data_dir, divided_grid=32):
+IM_EXTENSIONS = ['png', 'jpg', 'bmp']
+
+
+def read_data(data_dir, image_size, pixels_per_grid=32):
+    """
+    Load the data and preprocessing for YOLO detector
+    :param data_dir: str, path to the directory to read. It should include class_map, anchors, annotations
+    :image_size: tuple, image size for resizing images
+    :pixels_per_gird: int, the actual size of a grid
+    :return: X_set: np.ndarray, shape: (N, H, W, C).
+             y_set: np.ndarray, shape: (N, g_H, g_W, anchors, 5 + num_classes).
+    """
+    im_dir = os.path.join(data_dir, 'images')
+    class_map_path = os.path.join(data_dir, 'classes.json')
+    anchors_path = os.path.join(data_dir, 'anchors.json')
+    class_map = load_json(class_map_path)
+    anchors = load_json(anchors_path)
+    num_classes = len(class_map) + 1
+    grid_h, grid_w = [image_size[i] // pixels_per_grid for i in range(2)]
+    im_paths = []
+    for ext in IM_EXTENSIONS:
+        im_paths.extend(glob.glob(os.path.join(im_dir, '*.{}'.format(ext))))
+    anno_dir = os.path.join(data_dir, 'annotations')
+
+    images = []
+    labels = []
+
+    for im_path in im_paths:
+        # load image and resize image
+        im = imread(im_path)
+        im = np.array(im, dtype=np.float32)
+        im_origina_sizes = im.shape[:2]
+        im = resize(im, image_size)
+        if len(im.shape) == 2:
+            im = np.expand_dims(im, 2)
+            im = np.concatenate([im, im, im], -1)
+        images.append(im)
+
+        # load bboxes and reshape for yolo model
+        name = os.path.splitext(os.path.basename(im_path))[0]
+        anno_path = os.path.join(anno_dir, '{}.anno'.format(name))
+        with open(anno_path, 'r') as f:
+            anno = json.load(f)
+        label = np.zeros((grid_h, grid_w, len(anchors), 5 + num_classes))
+        for c_idx, c_name in class_map.items():
+            if c_name not in anno:
+                continue
+            for x_min, y_min, x_max, y_max in anno[c_name]:
+                oh, ow = im_origina_sizes
+                x_min, y_min, x_max, y_max = x_min / ow, y_min / oh, x_max / ow, y_max / oh
+                anchor_boxes = np.array(anchors) / np.array([ow, oh])
+                best_anchor = get_best_anchor(
+                    anchor_boxes, [x_max - x_min, y_max - y_min])
+                cx = int(np.floor(0.5 * (x_min + x_max) * grid_w))
+                cy = int(np.floor(0.5 * (y_min + y_max) * grid_h))
+                label[cy, cx, best_anchor, 0:4] = [x_min, y_min, x_max, y_max]
+                label[cy, cx, best_anchor, 4] = 1.0
+                label[cy, cx, best_anchor, 5 + int(c_idx)] = 1.0
+        labels.append(label)
+
+    X_set = np.array(images, dtype=np.float32)
+    y_set = np.array(labels, dtype=np.float32)
+
+    return X_set, y_set
+
+
+def load_json(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def get_best_anchor(anchors, box_wh):
+    box_wh = np.array(box_wh)
+    best_iou = 0
+    best_anchor = 0
+    for k, anchor in enumerate(anchors):
+        intersect_wh = np.maximum(np.minimum(box_wh, anchor), 0.0)
+        intersect_area = intersect_wh[0] * intersect_wh[1]
+        box_area = box_wh[0] * box_wh[1]
+        anchor_area = anchor[0] * anchor[1]
+        iou = intersect_area / (box_area + anchor_area - intersect_area)
+        if iou > best_iou:
+            best_iou = iou
+            best_anchor = k
+    return best_anchor
+
+
+class DataSet(object):
+
+    def __init__(self, images, labels=None):
+        """
+        Construct a new DataSet object.
+        :param images: np.ndarray, shape: (N, H, W, C)
+        :param labels: np.ndarray, shape: (N, g_H, g_W, anchors, 5 + num_classes).
+        """
+        if labels is not None:
+            assert images.shape[0] == labels.shape[0],\
+                ('Number of examples mismatch, between images and labels')
+        self._num_examples = images.shape[0]
+        self._images = images
+        self._labels = labels # NOTE: this can be None, if not given.
+        self._indices = np.arange(self._num_examples, dtype=np.uint) # image/label indices(can be permuted)
+        self._reset()
+
+    def _reset(self):
+        """Reset some variables."""
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def image(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    def next_batch(self, batch_size, shuffle=True, is_train=True):
+        """
+        Return the next 'batch_size' examples from this dataset.
+        :param batch_size: int, size of a single batch.
+        :param shuffle: bool, whether to shuffle the whole set while sampling a batch.
+        :param is_train: bool, current phase for sampling.
+        :return: batch_images: np.ndarray, shape: (N, H, W, C)
+                 batch_labels: np.ndarray, shape: (N, g_H, g_W, anchors, 5 + num_classes)
+        """
+
+        start_index = self._index_in_epoch
+
+        # Shuffle the dataset, for the first epoch
+        if self._epochs_completed == 0 and start_index == 0 and shuffle:
+            np.random.shuffle(self._indices)
+
+        # Go to the next epoch, if current index goes beyond the total number of examples
+        if start_index + batch_size > self._num_examples:
+            # Increment the number of epochs completed
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start_index
+            indices_rest_part = self._indices[start_index:self._num_examples]
+
+            # Shuffle the dataset, after finishing a single epoch
+            if shuffle:
+                np.random.shuffle(self._indices)
+
+            # Start the next epoch
+            start_index = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end_index = self._index_in_epoch
+            indices_new_part = self._indices[start_index:end_index]
+
+            images_rest_part = self.images[indices_rest_part]
+            images_new_part = self.images[indices_new_part]
+            batch_images = np.concatenate((images_rest_part, images_new_part), axis=0)
+            if self.labels is not None:
+                labels_rest_part = self.labels[indices_rest_part]
+                labels_new_part = self.labels[indices_new_part]
+                batch_labels = np.concatenate((labels_rest_part, labels_new_part), axis=0)
+            else:
+                batch_labels = None
+        else:
+            self._index_in_epoch += batch_size
+            end_index = self._index_in_epoch
+            indices = self._indices[start_index:end_index]
+            batch_images = self.images[indices]
+            if self.labels is not None:
+                batch_labels = self.labels[indices]
+            else:
+                batch_labels = None
+
+        return batch_images, batch_labels
