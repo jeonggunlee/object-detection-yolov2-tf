fix batch normalization error

datasets/data.py
@@ -45,8 +45,7 @@ def read_data(data_dir, image_size, pixels_per_grid=32):
         # load bboxes and reshape for yolo model
         name = os.path.splitext(os.path.basename(im_path))[0]
         anno_path = os.path.join(anno_dir, '{}.anno'.format(name))
-        with open(anno_path, 'r') as f:
-            anno = json.load(f)
+        anno = load_json(anno_path)
         label = np.zeros((grid_h, grid_w, len(anchors), 5 + num_classes))
         for c_idx, c_name in class_map.items():
             if c_name not in anno:
@@ -69,12 +68,10 @@ def read_data(data_dir, image_size, pixels_per_grid=32):
 
     return X_set, y_set
 
-
 def load_json(json_path):
     with open(json_path, 'r') as f:
         data = json.load(f)
     return data
-
 
 def get_best_anchor(anchors, box_wh):
     box_wh = np.array(box_wh)

learning/optimizers.py
@@ -27,7 +27,6 @@ class Optimizer(metaclass=ABCMeta):
 		self.batch_size = kwargs.pop('batch_size', 8)
 		self.num_epochs = kwargs.pop('num_epochs', 100)
 		self.init_learning_rate = kwargs.pop('init_learning_rate', 0.001)
-
 		self.learning_rate_placeholder = tf.placeholder(tf.float32)
 		self.optimize = self._optimize_op()
 
@@ -68,13 +67,11 @@ class Optimizer(metaclass=ABCMeta):
 
 		# Sample a single batch
 		X, y_true = self.train_set.next_batch(self.batch_size, shuffle=True)
-
 		# Compute the loss and make update
 		_, loss, y_pred = \
 			sess.run([self.optimize, self.model.loss, self.model.pred],
 				feed_dict={self.model.X: X, self.model.y: y_true, self.model.is_train: True, self.learning_rate_placeholder: self.curr_learning_rate})
-
-		return loss, y_true, y_pred
+		return loss, y_true, y_pred, X
 
 	def train(self, sess, save_dir='/tmp', details=False, verbose=True, **kwargs):
 		"""
@@ -103,10 +100,10 @@ class Optimizer(metaclass=ABCMeta):
 		# Start training loop
 		for i in range(num_steps):
 			# Perform a gradient update from a single minibatch
-			step_loss, step_y_true, step_y_pred = self._step(sess, **kwargs)
+			step_loss, step_y_true, step_y_pred, step_X = self._step(sess, **kwargs)
 			step_losses.append(step_loss)
 			# Perform evaluation in the end of each epoch
-			if (i+1) % num_steps_per_epoch == 0:
+			if (i + 1) % num_steps_per_epoch == 0:
 				# Evaluate model with current minibatch, from training set
 				sample_X, sample_y = self.train_set.sample_batch(20)
 				step_score = self.evaluator.score(sess, self.model, sample_X, sample_y)
@@ -201,16 +198,17 @@ class AdamOptimizer(Optimizer):
 
 	def _optimize_op(self, **kwargs):
 		"""
-		tf.train.MomentumOptimizer.minimize Op for a gradient update.
+		tf.train.AdamOptimizer.minimize Op for a gradient update.
 		:param kwargs: dict, extra arguments for optimizer.
 			-momentum: float, the momentum coefficent.
 		:return tf.Operation.
 		"""
 		momentum = kwargs.pop('momentum', 0.9)
-
+		extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 		update_vars = tf.trainable_variables()
-		return tf.train.AdamOptimizer(self.learning_rate_placeholder, momentum)\
-		.minimize(self.model.loss, var_list=update_vars)
+		with tf.control_dependencies(extra_update_ops):
+			train_op = tf.train.AdamOptimizer(self.learning_rate_placeholder, momentum).minimize(self.model.loss, var_list=update_vars)
+		return train_op
 
 	def _update_learning_rate(self, **kwargs):
 		"""

models/layers.py
@@ -82,7 +82,8 @@ def conv_layer(x, side_l, stride, out_depth, padding='SAME', use_bias=True, **kw
         return conv2d(x, filters, stride, padding=padding)
 
 def batchNormalization(x, is_train):
-    return tf.keras.layers.BatchNormalization()(x, training=is_train)
+    return tf.layers.batch_normalization(x, training=is_train, momentum=0.99, epsilon=0.001, center=True, scale=True)
+    # return tf.keras.layers.BatchNormalization()(x, training=is_train)
 
 def fc_layer(x, out_dim, **kwargs):
     """

models/nn.py
@@ -85,11 +85,11 @@ class YOLO(DetectNet):
 
     def __init__(self, input_shape, num_classes, anchors, **kwargs):
 
-        self.grid_size = grid_size = [x//32 for x in input_shape[:2]]
+        self.grid_size = grid_size = [x // 32 for x in input_shape[:2]]
         self.num_anchors = len(anchors)
         self.anchors = anchors
         self.y = tf.placeholder(tf.float32, [None] +
-                        [self.grid_size[0], self.grid_size[1], self.num_anchors, 5 + num_classes])
+                                [self.grid_size[0], self.grid_size[1], self.num_anchors, 5 + num_classes])
         super(YOLO, self).__init__(input_shape, num_classes, **kwargs)
 
     def _build_model(self, **kwargs):
@@ -141,7 +141,7 @@ class YOLO(DetectNet):
             d['conv4'] = conv_layer(d['leaky_relu3'], 1, 1, 64,
                                     padding='SAME', use_bias=False, weights_stddev=0.01)
             d['batch_norm4'] = batchNormalization(d['conv4'], is_train)
-            d['leaky_relu4'] = tf.nn.leaky_relu(d['conv4'], alpha=0.1)
+            d['leaky_relu4'] = tf.nn.leaky_relu(d['batch_norm4'], alpha=0.1)
         # (104, 104, 128) --> (104, 104, 64)
         print('layer4.shape', d['leaky_relu4'].get_shape().as_list())
 
@@ -227,7 +227,7 @@ class YOLO(DetectNet):
             d['leaky_relu13'] = tf.nn.leaky_relu(d['batch_norm13'], alpha=0.1)
             d['pool13'] = max_pool(d['leaky_relu13'], 2, 2, padding='SAME')
         # (26, 26, 256) --> (13, 13, 512)
-        print('pool13.shape', d['pool13'].get_shape().as_list())
+        print('layer13.shape', d['pool13'].get_shape().as_list())
 
         #conv14 - batch_norm14 - leaky_relu14
         with tf.variable_scope('layer14'):
@@ -292,7 +292,7 @@ class YOLO(DetectNet):
         # (13, 13, 1024) --> (13, 13, 1024)
         print('layer20.shape', d['leaky_relu20'].get_shape().as_list())
 
-        # concatenate layer21 and layer 13 using space to depth
+        # concatenate layer20 and layer 13 using space to depth
         with tf.variable_scope('layer21'):
             d['skip_connection'] = conv_layer(d['leaky_relu13'], 1, 1, 64,
                                               padding='SAME', use_bias=False, weights_stddev=0.01)
@@ -306,7 +306,7 @@ class YOLO(DetectNet):
         # (13, 13, 1024) --> (13, 13, 256+1024)
         print('layer21.shape', d['concat21'].get_shape().as_list())
 
-        #conv22 - leaky_relu22
+        #conv22 - batch_norm22 - leaky_relu22
         with tf.variable_scope('layer22'):
             d['conv22'] = conv_layer(d['concat21'], 3, 1, 1024,
                                      padding='SAME', use_bias=False, weights_stddev=0.01)
@@ -334,6 +334,8 @@ class YOLO(DetectNet):
         """
 
         loss_weights = kwargs.pop('loss_weights', [5, 5, 5, 0.5, 1.0])
+        ### DEBUG
+        # loss_weights = kwargs.pop('loss_weights', [1.0, 1.0, 1.0, 1.0, 1.0])
         grid_h, grid_w = self.grid_size
         num_classes = self.num_classes
         anchors = self.anchors
@@ -352,20 +354,24 @@ class YOLO(DetectNet):
         bwbh = tf.exp(twth) * pwph
 
         # for prediction
-        nxny, nwnh = bxby /grid_wh, bwbh / grid_wh
+        nxny, nwnh = bxby / grid_wh, bwbh / grid_wh
         nx1ny1, nx2ny2 = nxny - 0.5 * nwnh, nxny + 0.5 * nwnh
-        self.pred_y = tf.concat((nx1ny1, nx2ny2, confidence, class_probs), axis=-1)
+        self.pred_y = tf.concat(
+            (nx1ny1, nx2ny2, confidence, class_probs), axis=-1)
 
         # calculating IoU for metric
-        num_objects = tf.reduce_sum(self.y[...,4:5], axis=[1,2,3,4])
+        num_objects = tf.reduce_sum(self.y[..., 4:5], axis=[1, 2, 3, 4])
         max_nx1ny1 = tf.maximum(self.y[..., 0:2], nx1ny1)
         min_nx2ny2 = tf.minimum(self.y[..., 2:4], nx2ny2)
         intersect_wh = tf.maximum(min_nx2ny2 - max_nx1ny1, 0.0)
         intersect_area = tf.reduce_prod(intersect_wh, axis=-1)
-        gt_box_area = tf.reduce_prod(self.y[..., 2:4] - self.y[...,0:2], axis=-1)
+        intersect_area = tf.where(tf.equal(intersect_area, 0.0), tf.zeros_like(intersect_area), intersect_area)
+        gt_box_area = tf.reduce_prod(
+            self.y[..., 2:4] - self.y[..., 0:2], axis=-1)
         box_area = tf.reduce_prod(nx2ny2 - nx1ny1, axis=-1)
-        iou = tf.truediv(intersect_area, (gt_box_area + box_area - intersect_area))
-        iou = tf.reduce_sum(iou, axis=[1,2,3])
+        iou = tf.truediv(
+            intersect_area, (gt_box_area + box_area - intersect_area))
+        iou = tf.reduce_sum(iou, axis=[1, 2, 3])
         self.iou = tf.truediv(iou, num_objects)
 
         gt_bxby = 0.5 * (self.y[..., 0:2] + self.y[..., 2:4]) * grid_wh
@@ -382,18 +388,20 @@ class YOLO(DetectNet):
             tf.square(tf.sqrt(gt_bwbh) - tf.sqrt(bwbh))
         loss_resp_conf = loss_weights[2] * resp_mask * \
             tf.square(gt_confidence - confidence)
-        loss_no_resp_conf = loss_weights[3] * resp_mask * \
+        loss_no_resp_conf = loss_weights[3] * no_resp_mask * \
             tf.square(gt_confidence - confidence)
         loss_class_probs = loss_weights[4] * resp_mask * \
             tf.square(gt_class_probs - class_probs)
 
         merged_loss = tf.concat((
-        						loss_bxby,
+            					loss_bxby,
                                 loss_bwbh,
-                                loss_resp_conf + loss_no_resp_conf,
+                                loss_resp_conf,
+                                loss_no_resp_conf,
                                 loss_class_probs
                                 ),
                                 axis=-1)
+        self.merged_loss = merged_loss
         total_loss = tf.reduce_sum(merged_loss, axis=-1)
         total_loss = tf.reduce_mean(total_loss)
 
@@ -421,7 +429,8 @@ class YOLO(DetectNet):
                 image = images[i * batch_size:]
             else:
                 image = images[i * batch_size:(i + 1) * batch_size]
-            bbox = sess.run(self.pred_y, feed_dict={self.X: image, self.is_train: False})
+            bbox = sess.run(self.pred_y, feed_dict={
+                            self.X: image, self.is_train: False})
             bbox = np.reshape(bbox, (bbox.shape[0], -1, bbox.shape[-1]))
             bboxes.append(bbox)
         bboxes = np.concatenate(bboxes, axis=0)