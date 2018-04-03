dc48c3fe1764415cc90278a2ac1c2aff172b621a
JongsooKeum <keum.jongsoo@sualab.com>
2018-04-03 18:56:51 +0900

    minor fix

--- learning/utils.py
+++ learning/utils.py
@@ -134,7 +134,7 @@ def _cal_overlap(a, b):
 
     return intersection / ua
 
-def draw_pred_boxes(image, pred_boxes, class_map, predict=None):
+def draw_pred_boxes(image, pred_boxes, class_map, text=True, score=False):
     im_h, im_w = image.shape[:2]
     output = image.copy()
     for box in pred_boxes:
@@ -149,13 +149,12 @@ def draw_pred_boxes(image, pred_boxes, class_map, predict=None):
                       (x_max, y_max), color, line_width)
         output = cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0)
 
-        class_name = class_map[str(class_idx)]
-        y_offset = -6
-        text_size = 0.6
-        text_line_width = 1
-        if predict is not None:
-            class_name = str(round(np.max(box[5:]), 3))
-        output = cv2.putText(output, class_name, (x_min + 4, y_min + y_offset),
-                             cv2.FONT_HERSHEY_DUPLEX, text_size, color, text_line_width)
+        if text:
+            p_text = str(round(np.max(box[5:]), 3)) if score else class_map[str(class_idx)]
+            y_offset = -6
+            text_size = 0.6
+            text_line_width = 1
+            output = cv2.putText(output, p_text, (x_min + 4, y_min + y_offset),
+                                 cv2.FONT_HERSHEY_DUPLEX, text_size, color, text_line_width)
 
     return output
